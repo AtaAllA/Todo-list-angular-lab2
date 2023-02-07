10f631b659fd8995390bff28cae339fb4c33753e
AtaAllA <100690634+AtaAllA@users.noreply.github.com>
2023-02-07 21:31:12 +0200

    Angular todo list lab2

---
+++ to-do-list/src/app/app.component.ts
@@ -0,0 +1,42 @@
+import { Item } from "./item";
+
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class AppComponent {
+  title = 'todo-list';
+
+  filter: 'all' | 'active' | 'done' = 'all';
+
+  allItems = [
+    { description: 'eat', done: true },
+    ];
+
+  get items() {
+    if (this.filter === 'all') {
+      return this.allItems;
+    }
+    return this.allItems.filter((item) => this.filter === 'done' ? item.done : !item.done);
+  }
+
+  addItem(description: string) {
+    this.allItems.unshift({
+      description,
+      done: false
+    });
+  }
+
+  remove(item: Item) {
+    this.allItems.splice(this.allItems.indexOf(item), 1);
+  }
+
+  
+  
+
+}
